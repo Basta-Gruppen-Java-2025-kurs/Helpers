071822a381902a5ad25e27e3dc790e693bcac21e
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-10-09 09:40:14 +0200

    + Add MenuBuilder

---
+++ src/MenuBuilder.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MenuBuilder {
+    private final ArrayList<String> items = new ArrayList<>();
+    private final ArrayList<Runnable> callbacks = new ArrayList<>();
+    private String header = "Please select an option.";
+    private boolean singleShot = false;
+
+    public MenuBuilder(String header, String exitItem) {
+        this.header = header;
+        items.add(exitItem);
+    }
+
+    public MenuBuilder(String exitItem) {
+        items.add(exitItem);
+    }
+
+    public MenuBuilder() {
+        items.add("Exit");
+    }
+
+    public MenuBuilder setHeader(String header) {
+        this.header = header;
+        return this;
+    }
+
+    public MenuBuilder setSingleShot(boolean singleShot) {
+        this.singleShot = singleShot;
+        return this;
+    }
+
+    public MenuBuilder addItem(String item, Runnable callback) {
+        items.add(item);
+        callbacks.add(callback);
+        return this;
+    }
+
+    public void runMenu() {
+        TextMenu.menuLoop(header, items.toArray(new String[0]), callbacks.toArray(new Runnable[0]), singleShot);
+    }
+
+    public static void main(String[] args) {
+        MenuBuilder menu = new MenuBuilder();
+        menu    .setHeader("Test menu");
+        menu    .addItem("First", () -> System.out.println("First item"))
+                .addItem("Second", () -> System.out.println("You never guess"))
+                .runMenu();
+    }
+
+}
